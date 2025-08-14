2ad7d939564927af7b36cc518935afe6a8e3bb56
Sawyer Ma <mmw-solutions@web.de>
2025-08-14 21:24:41 +0200

    12_08_2025_Gui_Umsetzung_9

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    react()
+  ],
   server: {
     host: "::",
     port: 8080,
@@ -14,10 +18,14 @@ export default defineConfig({
       }
     }
   },
-  plugins: [react()],
   resolve: {
     alias: {
-      "@": "/src"
+      "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    }
+  }
 })
